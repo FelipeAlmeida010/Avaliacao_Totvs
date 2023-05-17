b3b05af57d502bbc6c3911e19cbf7e320c9a02d6
Felipe Almeida <felipealmeida0404@gmail.com>
2023-05-17 18:57:17 -0300

    atualizacao01

---
+++ teste.js
@@ -0,0 +1,20 @@
+/*var employee={
+    name: "luke",
+    lastName:"Skun",
+    skills:{ruby: "high", java: "low", python: "nome"}
+}
+for (var skill in employee.skills){
+    console.log(`${skill}: ${employee.skills[skill]}`)
+}*/
+
+/*var numbers = [4, 4, 8, 15, 16, 16, 23, 42]
+
+var modifiedNumbers = numbers.filter(function(elem, index, array){
+    return array.indexOf(elem) ===index
+})
+console.log(modifiedNumbers)*/
+
+var a = [[1,2,2,4,], [5,6,7,8],[9,10,11,12],[13,14,15,16]]
+
+for (var i =0, j =3; i <= 3; i++, j--)
+console.log(a[i][j])
